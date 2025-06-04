e55a7dc8d7895ccd58aeb44bfc97fadbed6f96ac
lzieniew <lukzieniewicz@gmail.com>
2025-06-05 00:09:27 +0200

    Changes

---
+++ run.sh
@@ -0,0 +1,4 @@
+uv venv
+uv pip install .
+uv pip install gradio
+uv run gradio_tts_app.py
--- src/chatterbox/vc.py
+++ src/chatterbox/vc.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 import librosa
 import torch
-import perth
+
+# import perth
 from huggingface_hub import hf_hub_download
 from safetensors.torch import load_file
 
@@ -21,12 +22,12 @@ class ChatterboxVC:
         self,
         s3gen: S3Gen,
         device: str,
-        ref_dict: dict=None,
+        ref_dict: dict = None,
     ):
         self.sr = S3GEN_SR
         self.s3gen = s3gen
         self.device = device
-        self.watermarker = perth.PerthImplicitWatermarker()
+        # self.watermarker = perth.PerthImplicitWatermarker()
         if ref_dict is None:
             self.ref_dict = None
         else:
@@ -36,38 +37,40 @@ class ChatterboxVC:
             }
 
     @classmethod
-    def from_local(cls, ckpt_dir, device) -> 'ChatterboxVC':
+    def from_local(cls, ckpt_dir, device) -> "ChatterboxVC":
         ckpt_dir = Path(ckpt_dir)
-        
+
         # Always load to CPU first for non-CUDA devices to handle CUDA-saved models
         if device in ["cpu", "mps"]:
-            map_location = torch.device('cpu')
+            map_location = torch.device("cpu")
         else:
             map_location = None
-            
+
         ref_dict = None
         if (builtin_voice := ckpt_dir / "conds.pt").exists():
             states = torch.load(builtin_voice, map_location=map_location)
-            ref_dict = states['gen']
+            ref_dict = states["gen"]
 
         s3gen = S3Gen()
-        s3gen.load_state_dict(
-            load_file(ckpt_dir / "s3gen.safetensors"), strict=False
-        )
+        s3gen.load_state_dict(load_file(ckpt_dir / "s3gen.safetensors"), strict=False)
         s3gen.to(device).eval()
 
         return cls(s3gen, device, ref_dict=ref_dict)
 
     @classmethod
-    def from_pretrained(cls, device) -> 'ChatterboxVC':
+    def from_pretrained(cls, device) -> "ChatterboxVC":
         # Check if MPS is available on macOS
         if device == "mps" and not torch.backends.mps.is_available():
             if not torch.backends.mps.is_built():
-                print("MPS not available because the current PyTorch install was not built with MPS enabled.")
+                print(
+                    "MPS not available because the current PyTorch install was not built with MPS enabled."
+                )
             else:
-                print("MPS not available because the current MacOS version is not 12.3+ and/or you do not have an MPS-enabled device on this machine.")
+                print(
+                    "MPS not available because the current MacOS version is not 12.3+ and/or you do not have an MPS-enabled device on this machine."
+                )
             device = "cpu"
-            
+
         for fpath in ["s3gen.safetensors", "conds.pt"]:
             local_path = hf_hub_download(repo_id=REPO_ID, filename=fpath)
 
@@ -77,8 +80,10 @@ class ChatterboxVC:
         ## Load reference wav
         s3gen_ref_wav, _sr = librosa.load(wav_fpath, sr=S3GEN_SR)
 
-        s3gen_ref_wav = s3gen_ref_wav[:self.DEC_COND_LEN]
-        self.ref_dict = self.s3gen.embed_ref(s3gen_ref_wav, S3GEN_SR, device=self.device)
+        s3gen_ref_wav = s3gen_ref_wav[: self.DEC_COND_LEN]
+        self.ref_dict = self.s3gen.embed_ref(
+            s3gen_ref_wav, S3GEN_SR, device=self.device
+        )
 
     def generate(
         self,
@@ -88,11 +93,13 @@ class ChatterboxVC:
         if target_voice_path:
             self.set_target_voice(target_voice_path)
         else:
-            assert self.ref_dict is not None, "Please `prepare_conditionals` first or specify `target_voice_path`"
+            assert self.ref_dict is not None, (
+                "Please `prepare_conditionals` first or specify `target_voice_path`"
+            )
 
         with torch.inference_mode():
             audio_16, _ = librosa.load(audio, sr=S3_SR)
-            audio_16 = torch.from_numpy(audio_16).float().to(self.device)[None, ]
+            audio_16 = torch.from_numpy(audio_16).float().to(self.device)[None,]
 
             s3_tokens, _ = self.s3gen.tokenizer(audio_16)
             wav, _ = self.s3gen.inference(
@@ -100,5 +107,6 @@ class ChatterboxVC:
                 ref_dict=self.ref_dict,
             )
             wav = wav.squeeze(0).detach().cpu().numpy()
-            watermarked_wav = self.watermarker.apply_watermark(wav, sample_rate=self.sr)
-        return torch.from_numpy(watermarked_wav).unsqueeze(0)
+            # watermarked_wav = self.watermarker.apply_watermark(wav, sample_rate=self.sr)
+        return torch.from_numpy(wav).unsqueeze(0)
+
